2a4e7d3377dbf9ab95dfe996373866d035d3fc3c
Mikhaylo <yunko@hotmail.co.uk>
2020-11-03 21:33:06 +0200

    First commit from home

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4 / 0);
+console.log("str" * 9);
+
+const persone = "5";
+
+const bool = false;
+
+// console.log(something); // ReferenceError: something is not defined
+
+let end;
+
+console.log(end);
+
+const obj = {
+	name: "John",
+	age: 25,
+	isMarried: false,
+};
+
+console.log(obj.name);
+// console.log(obj["name"]);
+
+let arr = ["plum.png", "orange.jpg", 6, "apple.bpm", {}, []];
+console.log(arr[2]);
